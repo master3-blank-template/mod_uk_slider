f07b3611d61edd09ba3c3e824c2adcb36e2f24f0
AlekVolsk <alekvolsk@yandex.ru>
2019-02-22 19:56:12 +0400

    v1.2.0

--- mod_uk_slider.php
+++ mod_uk_slider.php
@@ -1,19 +1,20 @@
 <?php defined('_JEXEC') or die;
 /*
  * @package     mod_uk_slider
- * @copyright   Copyright (C) 2018 Aleksey A. Morozov (AlekVolsk). All rights reserved.
+ * @copyright   Copyright (C) 2019 Aleksey A. Morozov (AlekVolsk). All rights reserved.
  * @license     GNU General Public License version 3 or later; see http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
+use Joomla\CMS\Helper\ModuleHelper;
+
 $vars = [
-    'slider_class', 'item_class', 'dotnav', 'slidenav', 
-    'velocity', 'autoplay', 'autoplay_interval', 'finite', 'pause_on_hover', 'index', 'center', 'sets', 
+    'slider_class', 'item_class', 'dotnav', 'slidenav',
+    'velocity', 'autoplay', 'autoplay_interval', 'finite', 'pause_on_hover', 'index', 'center', 'sets',
     'grid', 'grid_divider', 'cols', 'cols_all', 'cols_s', 'cols_m', 'cols_l', 'cols_xl',
     'items'
 ];
 
-foreach ($vars as $var)
-{
+foreach ($vars as $var) {
     $$var = $params->get($var);
 }
 
@@ -22,8 +23,7 @@ $item_class = trim($item_class);
 
 $sw_params = [];
 if ((int)$velocity > 1) $sw_params[] = 'velocity:' . (int)$velocity;
-if ($autoplay)
-{
+if ($autoplay) {
     $sw_params[] = 'autoplay:true';
     if ((int)$autoplay_interval != 7000 && (int)$autoplay_interval > 0) $sw_params[] = 'autoplay_interval:' . (int)$autoplay_interval;
 }
@@ -35,13 +35,11 @@ if ((int)$sets > 0) $sw_params[] = 'sets:true';
 $sw_params = $sw_params ? '="' . implode(';', $sw_params) . '"' : '';
 
 $classes = [];
-if ($grid != '')
-{
+if ($grid != '') {
     $classes[] = $grid;
     if ((int)$grid_divider) $classes[] = 'uk-grid-divider';
 }
-if ((int)$cols)
-{
+if ((int)$cols) {
     $classes[] = $cols_all;
     $classes[] = $cols_s;
     $classes[] = $cols_m;
@@ -50,4 +48,6 @@ if ((int)$cols)
 }
 $classes = $classes ? ' ' . implode(' ', $classes) : '';
 
-require(JModuleHelper::getLayoutPath('mod_uk_slider', $params->get('layout', 'default')));
+if ($items) {
+    require(ModuleHelper::getLayoutPath('mod_uk_slider', $params->get('layout', 'default')));
+}
